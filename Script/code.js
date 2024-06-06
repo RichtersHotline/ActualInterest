c5e23e8f9dc579f530973fc02f15e40ae44fe642
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-06-06 10:59:35 +0200

    done

---
+++ Script/code.js
@@ -0,0 +1,18 @@
+let Answer = document.getElementById("Output")
+let Display = document.getElementById("Seconds")
+const interest = 20
+
+
+function Pricer(amount, percentage) {
+    percentage = "15%";
+    amount = 0.5;
+    slicer = (percentage.slice(0,2))
+    
+
+    console.log(amount * slicer)
+let UserInput = document.getElementById("UserInput").value
+// Display.style.display = "block"
+// Answer.innerHTML = UserInput * 20 + "%";
+
+
+}
